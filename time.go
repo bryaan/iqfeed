8371619fd4161fe5f8320cc27764e2431d51c5e1
Bryan Alexander Rivera <futurehorizon@gmail.com>
2017-10-13 01:30:07 -0400

    Update time.go

--- time.go
+++ time.go
@@ -12,7 +12,7 @@ type TimeMsg struct {
 // UnMarshall sends the data into the usable struct for consumption by the application.
 func (tm *TimeMsg) UnMarshall(d []byte, loc *time.Location) {
 	items := strings.Split(string(d), ",")
-	fmt.Println(string(items))
+	fmt.Println(items)
 	
 	t, _ := time.ParseInLocation("20060102 15:04:05", string(d), loc)
 	tm.TimeStamp = t
