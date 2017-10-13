389b2748ac9fb768b5dee21b11e93c7b69a5ca42
Bryan Alexander Rivera <futurehorizon@gmail.com>
2017-10-12 21:27:53 -0400

    Update levelII.go

--- levelII.go
+++ levelII.go
@@ -1,5 +1,10 @@
 package iqfeed
 
+import (
+	"strings"
+	"time"
+)
+
 // RegionalMsg A regional update message. See complete message definition in Regional Messages. (http://www.iqfeed.net/dev/api/docs/RegionalMessageFormat.cfm).
 type LevelII struct {
 	Raw           []string  // the  symbol that is being tracked
