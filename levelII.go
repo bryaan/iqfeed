b1b56e7939d7266567e1772fbbd2f486f5d5a739
Bryan Alexander Rivera <futurehorizon@gmail.com>
2017-10-12 20:39:32 -0400

    Create levelII.go
    Create LevelII struct

---
+++ levelII.go
@@ -0,0 +1,27 @@
+package iqfeed
+
+import (
+	"strings"
+	"time"
+)
+
+// RegionalMsg A regional update message. See complete message definition in Regional Messages. (http://www.iqfeed.net/dev/api/docs/RegionalMessageFormat.cfm).
+type LevelII struct {
+	Raw           []byte  // the  symbol that is being tracked
+}
+
+// UnMarshall sends the data into the usable struct for consumption by the application.
+//func (r *LevelII) UnMarshall(d []byte, loc *time.Location) {
+	//items := strings.Split(string(d), ",")
+	//r.Symbol = items[0]
+	//r.Exchange = items[1]
+	//r.RegBid = GetFloatFromStr(items[2])
+	//r.RegBidSize = GetIntFromStr(items[3])
+	//r.RegBidTime = GetTimeInHMS(items[4], loc)
+	//r.RegAsk = GetFloatFromStr(items[5])
+	//r.RegAskSize = GetIntFromStr(items[6])
+	//r.RegAskTime = GetTimeInHMS(items[7], loc)
+	//r.FractionDispCode = GetIntFromStr(items[8])
+	//r.DecPrecision = GetIntFromStr(items[9])
+	//r.MarketCenter = GetIntFromStr(items[10])
+//}
