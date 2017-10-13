a82a7d709534ca33ab3714f5bc101a976dd4a367
Bryan Alexander Rivera <futurehorizon@gmail.com>
2017-10-12 23:43:17 -0400

    Update levelII.go

--- levelII.go
+++ levelII.go
@@ -15,7 +15,7 @@ type LevelII struct {
 // UnMarshall sends the data into the usable struct for consumption by the application.
 func (r *LevelII) UnMarshall(d []byte, loc *time.Location) {
 // 	items := strings.Split(string(d), ",")
-	r.Raw = d
+	copy(r.Raw, d []byte)
 	
 	//r.Symbol = items[0]
 	//r.Exchange = items[1]
